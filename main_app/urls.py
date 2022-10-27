72d22dcccb98438396c4d56095bcb9ad62436388
achasek <chaseknieper@gmail.com>
2022-10-26 20:31:41 -0700

    full crud

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name="about"),
     path('critters/', views.critters_index, name="index"),
+    path('critters/<int:critter_id>/', views.critters_detail, name='detail'),
+    path('critters/create/', views.CritterCreate.as_view(), name="critters_create"),
+    path('critters/<int:pk>/update/', views.CritterUpdate.as_view(), name='critters_update'),
+    path('critters/<int:pk>/delete/', views.CritterDelete.as_view(), name='critters_delete'),
 ]
+
--- main_app/views.py
+++ main_app/views.py
@@ -1,24 +1,31 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Critter
+from django.views.generic import DetailView, ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
-class Critter:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, species, description, age):
-    self.name = name
-    self.species = species
-    self.description = description
-    self.age = age
-
-critters = [
-  Critter('Lolo', 'Prarie Dog', 'has the bubonic plague', 3),
-  Critter('Sachi', 'California Condor', 'Super fast. Crazy endangered tho', 0),
-  Critter('Raven', 'Mountain Lion', 'Almost hunted me down as I was hiking the rim before I tamed him', 4)
-]
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>AYYYE</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
 def critters_index(request):
-    return render(request, 'critters/index.html', { 'critters': critters })
+  critters = Critter.objects.all()
+  return render(request, 'critters/index.html', { 'critters': critters })
+
+def critters_detail(request, critter_id):
+  critter = Critter.objects.get(id=critter_id)
+  return render(request, 'critters/detail.html', { 'critter': critter })
+
+class CritterCreate(CreateView):
+  model = Critter
+  fields = '__all__'
+
+class CritterUpdate(UpdateView):
+  model = Critter
+  fields = ['species', 'description', 'age']
+
+class CritterDelete(DeleteView):
+  model = Critter
+  success_url = '/critters/'
